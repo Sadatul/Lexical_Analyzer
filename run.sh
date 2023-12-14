9f37bb01cf343dbf3fda867bde5a01ea14737722
Sadatul <sadatulislamsadi@gmail.com>
2023-12-14 13:14:40 +0600

    Added support for string and constant character errors

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ cppfile="${filename%.*}"
 # echo $cppfile
 
 mkdir -p ./output
-flex -o ./output/$cppfile.c $1 
-g++ ./output/$cppfile.c -lfl -o ./output/$cppfile.out
+flex -o ./output/$cppfile.cpp $1 
+g++ ./output/$cppfile.cpp -lfl -o ./output/$cppfile.out
 ./output/$cppfile.out $2
